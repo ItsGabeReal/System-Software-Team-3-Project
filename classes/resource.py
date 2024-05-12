9fe9622668ebfc42a0933613c501921559990071
Gabriel Wilson <44685241+ItsGabeReal@users.noreply.github.com>
2024-05-12 10:45:10 -0400

    Remove literal from resource.py

--- classes/resource.py
+++ classes/resource.py
@@ -4,10 +4,8 @@ Resource classes for ores and trees.
 
 from typing import Literal
 
-type resource_type = Literal['iron', 'copper', 'wood']
-
 class Resource:
-    def __init__(self, type: resource_type, location: tuple[int, int]) -> None:
+    def __init__(self, type: str, location: tuple[int, int]) -> None:
         self.health: int = 100 # Gets destroyed when health reaches zero
         self.type = type
         self.location = location
