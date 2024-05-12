972b48832516ae4723245ec8bfee4ac537f3363b
Gabriel Wilson <44685241+ItsGabeReal@users.noreply.github.com>
2024-05-12 10:45:37 -0400

    ...and remove import

--- classes/resource.py
+++ classes/resource.py
@@ -2,8 +2,6 @@
 Resource classes for ores and trees.
 '''
 
-from typing import Literal
-
 class Resource:
     def __init__(self, type: str, location: tuple[int, int]) -> None:
         self.health: int = 100 # Gets destroyed when health reaches zero
